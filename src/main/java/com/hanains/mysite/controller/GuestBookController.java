43d5635dd01d22e7300b2a8e77fb2029d7fd28e8
bit-user <bit-user@bit>
2015-12-18 13:58:16 +0900

    Logging, AOP 추가 완료!

--- src/main/java/com/hanains/mysite/controller/GuestBookController.java
+++ src/main/java/com/hanains/mysite/controller/GuestBookController.java
@@ -1,5 +1,7 @@
 package com.hanains.mysite.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,16 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hanains.mysite.service.GuestBookService;
 import com.hanains.mysite.vo.GuestBookVo;
-
 @Controller
 @RequestMapping("/guestbook")
 public class GuestBookController {
 
+	private static final Log LOG = LogFactory.getLog( GuestBookController.class );
+	
 	@Autowired
 	private GuestBookService service;
 	
 	@RequestMapping("/")
 	public String list(Model model){
+		LOG.error("GuestBookController.list");
 		model.addAttribute("list",service.getList());
 		return "/guestbook/list";
 	}
